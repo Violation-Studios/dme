2c08beafe3fd7c2eadd2357dfbc3faa1ed8288c5
nikolaoslibero <nikolaos@violation.studio>
2024-12-01 03:29:29 -0500

    adds hardcoded actions and descriptions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-const fn main() {}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(1i32, 1i32);
+fn main() {
+    let status_updates: Vec<&str> = vec![
+        "A tavern and a smithy are nearby.",
+        "You decide to enter the tavern.",
+        "The tavern is entirely empty of people, but the bar seems well-stocked.",
+        "You decide to make yourself a drink. You make a 'Rusty Nail', a drink consisting of Scotch Whisky and Drambuie. The irony of mixing whisky with whisky is not lost on you.",
+    ];
+    #[expect(clippy::print_stdout, reason = "I do what I want.")]
+    for update in status_updates {
+        println!("{update}");
     }
 }
+
+#[cfg(test)]
+mod tests {}
